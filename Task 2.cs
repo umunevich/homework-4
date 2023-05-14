067e5a3a906e349296b47adcd89c40976902aad6
Yana <125117368+umunevich@users.noreply.github.com>
2023-05-14 15:55:22 +0300

    Task 2.cs

--- Task 2.cs
+++ Task 2.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int Lines, Columns, max, min;
+                        int Lines, Columns, max, min;
 
             Console.WriteLine("Enter the size of array: ");
             Console.Write("Number of lines - ");
@@ -19,39 +19,40 @@ namespace ConsoleApp1
             Columns = Convert.ToInt32(Console.ReadLine());
             int[,] arr = new int[Lines, Columns];
             Console.WriteLine($"Enter an array of {Lines*Columns} elements:");
-            for (int i = 0; i < Columns; i++)
+            for (int i = 0; i < Lines; i++)
             {
-                for (int j = 0; j < Lines; j++)
+                for (int j = 0; j < Columns; j++)
                 {
-                    arr[j, i] = Convert.ToInt32(Console.ReadLine());
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
 
             max = arr[0, 0];
-            for (int i = 0; i < Columns; i++)
+            for (int i = 0; i < Lines; i++)
             {
-                for (int j = 0; j < Lines; j++)
+                for (int j = 0; j < Columns; j++)
                 {
-                    if (max < arr[j, i])
+                    if (max < arr[i, j])
                     {
-                        max = arr[j, i];
+                        max = arr[i, j];
                     }
                 }
             }
 
             min = arr[0, 0];
-            for (int i = 0; i < Columns; i++)
+            for (int i = 0; i < Lines; i++)
             {
-                for (int j = 0; j < Lines; j++)
+                for (int j = 0; j < Columns; j++)
                 {
-                    if (min > arr[j, i])
+                    if (min > arr[i, j])
                     {
-                        min = arr[j, i];
+                        min = arr[i, j];
                     }
                 }
             }
 
             Console.WriteLine($"The summa of max {max} and min {min} is {max+min}");
+
             Console.ReadKey(false);
         }
     }
